465926620f1aa779f94a00e3f3b8fecd06998c23
Alex Hardt <hardt.alex@gmail.com>
2015-02-17 10:50:40 -0600

    twitter dm feature done, working on improving it

--- micro_blogger/micro_blogger.rb
+++ micro_blogger/micro_blogger.rb
@@ -16,9 +16,38 @@ class MicroBlogger
     end
   end
 
+  def dm(target, message)
+
+    #logic to verify you can DM someone
+
+
+    puts "Trying to send #{target} this direct message:"
+    puts message
+    message = "d @#{target} #{message}"
+    tweet(message)
+  end
+
+  def run
+    puts "Welcome to the JSL Twitter Client!"
+    command = ""
+    while command != "q"
+      printf "enter command: "
+      input = gets.chomp
+      parts = input.split(" ")
+      command = parts[0]
+      case command
+        when 'q' then puts "Goodbye!"
+        when 't' then  tweet(parts[1..-1].join(" "))
+        when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+          else
+          puts "Sorry, I don't know how to #{command}"
+      end
+    end
+  end
+
 end #ends class
 
 blogger = MicroBlogger.new
 
 
-blogger.tweet("more than 40 test- asodnassdsdsdsdsdsdsdsdsdlansdofansdf;lnasdf;lnads;flnads;lfnasdl;fndlnfal;dsnfal;sdnflads sdksdlsdnkdslkn adflknasdn end here? is this 140 characters?")
+blogger.run#("more than 40 test- asodnassdsdsdsdsdsdsdsdsdlansdofansdf;lnasdf;lnads;flnads;lfnasdl;fndlnfal;dsnfal;sdnflads sdksdlsdnkdslkn adflknasdn end here? is this 140 characters?")
