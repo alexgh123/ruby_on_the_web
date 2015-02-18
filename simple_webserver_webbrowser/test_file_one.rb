4de732f904903684f063c953f27643922ce7c4d3
Alex Hardt <hardt.alex@gmail.com>
2015-02-17 18:21:44 -0600

    Large commit message because I didn't understand a lot/want to capture what i learned:
    Basically, i had to write two files, one was the server and one was the client, once i ran the server in file one, it was running on a continuous loop. then when i ran file two, the client file, it was able to querry the server and get the borring information from it (time/date and a message that says, closing connection, bye). but it was super interesting how it worked. next i'm working on using a server from the big bad internet, not my safe humble computer

---
+++ simple_webserver_webbrowser/test_file_one.rb
@@ -0,0 +1,13 @@
+#a simple server
+
+require 'socket'
+
+server = TCPServer.open(2000)
+loop {
+
+  client = server.accept
+  client.puts(Time.now.ctime)
+  client.puts "Closing the connection, bye!"
+  client.close
+
+}
---
+++ simple_webserver_webbrowser/test_file_two.rb
@@ -0,0 +1,13 @@
+#a simple client
+
+require 'socket'
+
+hostname = 'localhost'
+port = 2000
+
+s = TCPSocket.open(hostname, port)
+
+while line = s.gets
+  puts line.chop
+end
+s.close
