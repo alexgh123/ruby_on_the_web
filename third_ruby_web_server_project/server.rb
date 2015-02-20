d96acafd8a320c978e7906191a46c618aa7bf824
Alex Hardt <hardt.alex@gmail.com>
2015-02-20 09:14:45 -0600

    adding comments to better understand code

--- third_ruby_web_server_project/server.rb
+++ third_ruby_web_server_project/server.rb
@@ -1,9 +1,12 @@
 require 'socket'
 webserver = TCPServer.new('127.0.0.1', 7125)#^ connection to server(the fridge) (not webserver(waiter)) #so to connect to net, do i need to have a waiter and a fridge connect?
-while (session = webserver.accept)
+while (session = webserver.accept) #handles server requests until ctrl c
    session.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
+   session.print "session.print is: #{session.print}"#alex
    request = session.gets
+   session.print "request is #{request}" #alex insertion
    trimmedrequest = request.gsub(/GET\ \//, '').gsub(/\ HTTP.*/, '')
+   session.print "trimmedrequest is #{trimmedrequest}" #alex insertion
    filename = trimmedrequest.chomp
    if filename == ""
       filename = "index.html"
