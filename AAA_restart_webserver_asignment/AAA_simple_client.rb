a6fd542c7cda44436ddcbfbd1f9431d54098719d
Alex Hardt <hardt.alex@gmail.com>
2015-02-20 10:04:07 -0600

    playing around with it

--- AAA_restart_webserver_asignment/AAA_simple_client.rb
+++ AAA_restart_webserver_asignment/AAA_simple_client.rb
@@ -1,11 +1,45 @@
-require 'socket'
+# require 'socket'
 
-hostname = 'localhost' #instead of www.google.com
-port = 2000
+# hostname = 'localhost' #instead of www.google.com
+# port = 2000
 
-s = TCPSocket.open(hostname, port)
+# s = TCPSocket.open(hostname, port)
 
-while line = s.gets
-  puts line.chop
+# while line = s.gets
+#   puts line.chop
+# end
+# s.close
+
+
+
+#alt version below
+
+
+
+# require 'socket'
+
+# host = 'www.tutorialspoint.com'     # The web server
+# port = 80                           # Default HTTP port
+# path = "/index.htm"                 # The file we want
+
+# # This is the HTTP request we send to fetch a file
+# request = "GET #{path} HTTP/1.0\r\n\r\n"
+
+# socket = TCPSocket.open(host,port)  # Connect to server
+# socket.print(request)               # Send request
+# response = socket.read              # Read complete response
+# # Split response at first blank line into headers and body
+# headers,body = response.split("\r\n\r\n", 2)
+# print body                          # And display it
+
+require 'net/http'
+
+host = 'www.tutorialspoint.com'
+path = '\index.htm'
+http = Net::HTTP.new(host)
+headers, body = http.get(path)
+if headers.code == "200"
+  print body
+else
+  puts "#{headers.code} eh... #{headers.message}"
 end
-s.close
